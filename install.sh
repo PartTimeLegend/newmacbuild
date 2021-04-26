1ad72eae8898e36968aa9a2209d974140cb40e30
Antony Bailey <hi@antonybailey.net>
2021-04-26 11:10:00 +0100

    Actually use filenames

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-if test -f "$FILE"; then
+if test -f "installbrew.sh"; then
   .\installbrew.sh
 else
   curl -sSL https://raw.githubusercontent.com/PartTimeLegend/newmacbuild/master/installbrew.sh | bash
 fi
 
-if test -f "$FILE"; then
+if test -f "installapps.sh"; then
   .\installapps.sh
 else
   curl -sSL https://raw.githubusercontent.com/PartTimeLegend/newmacbuild/master/installapps.sh | bash
