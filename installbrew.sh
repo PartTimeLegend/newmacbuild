29d5fce444db668e88635541705dc786e9a23d3b
Antony Bailey <hi@antonybailey.net>
2021-04-26 10:24:26 +0100

    Cask

--- installbrew.sh
+++ installbrew.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew tap caskroom/cask
-brew install caskroom/cask/brew-cask
+brew tap homebrew/cask
+brew install homebrew/cask/brew-cask
