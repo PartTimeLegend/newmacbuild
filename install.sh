927021f6ad96f2df1383f699b410b718dc031c0b
Antony Bailey <hi@antonybailey.net>
2021-04-26 11:04:05 +0100

    Rhymes be the proof while I'm drinkin' 90 proof Huh vodka, no OJ, no straw, When you give it to me aiy, give it to me raw

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 if test -f "$FILE"; then
   .\installbrew.sh
 else
-  curl -sSL https://github.com/PartTimeLegend/newmacbuild/blob/master/installbrew.sh | bash
+  curl -sSL https://raw.githubusercontent.com/PartTimeLegend/newmacbuild/master/installbrew.sh | bash
 fi
 
 if test -f "$FILE"; then
   .\installapps.sh
 else
-  curl -sSL https://github.com/PartTimeLegend/newmacbuild/blob/master/installapps.sh | bash
+  curl -sSL https://raw.githubusercontent.com/PartTimeLegend/newmacbuild/master/installapps.sh | bash
 fi
