fd52a05cd98ef017f3782472f2ce8a4bfe053775
Antony Bailey <hi@antonybailey.net>
2021-04-26 10:56:59 +0100

    Add remote install

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-.\installbrew.sh
-.\installapps.sh
+if test -f "$FILE"; then
+  .\installbrew.sh
+else
+  curl -sSL https://github.com/PartTimeLegend/newmacbuild/blob/master/installbrew.sh | bash
+fi
+
+if test -f "$FILE"; then
+  .\installapps.sh
+else
+  curl -sSL https://github.com/PartTimeLegend/newmacbuild/blob/master/installapps.sh | bash
+fi
